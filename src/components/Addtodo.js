19edc144a65e2162e5b83175fa9cf4510fbcb858
ndewon <duniandewon@gmail.com>
2019-05-14 22:09:11 +0700

    This a todo list app v1.0.0 I created following Brad Traversy's React crash course video on YouTube

---
+++ src/components/Addtodo.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Addtodo extends Component {
+  state = {
+    title: ''
+  }
+
+  onChange = (e) => this.setState({[e.target.name]: e.target.value});
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.addTodo(this.state.title);
+    this.setState({title: ''});
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit} style={{display: 'flex'}}>
+        <input type="text"
+        name="title"
+        value={this.state.title}
+        onChange={this.onChange}
+        style={{flex: '10', padding: '5px'}}
+        placeholder="Add todo" />
+        <input type="submit" value="Submit" className="btn" style={{flex: '1'}} />
+      </form>
+    )
+  }
+}
+
+// Prop Types
+Addtodo.propTypes = {
+  addTodo: PropTypes.func.isRequired
+}
+
+export default Addtodo
---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This a todo list app v1.0.0 I created following Brad Traversy's React crash course video on YouTube</p>
+    </React.Fragment>
+  )
+}
